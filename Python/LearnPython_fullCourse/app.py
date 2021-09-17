691914092212c4aeb4a83ca6241aa799c4e0e709
Kelvin Ferri Brancalhao <kelvinbrancalhao@gmail.com>
2021-09-17 16:55:23 -0300

    adicionando Curso de Python da Freecode camp

---
+++ Python/LearnPython_fullCourse/app.py
@@ -0,0 +1,12 @@
+#curso de python - learn python full course for beginners [tutorial];
+
+caracter_name = "Jon"
+caracter_age = "35"
+
+print("the name of my caracter is " + caracter_name)
+print("the age of my caracter is " + caracter_age)
+print("")
+print("")
+print("")
+print("")
+#parei em 33 minutos no curso bora que bora
