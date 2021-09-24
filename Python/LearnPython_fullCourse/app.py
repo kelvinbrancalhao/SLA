2b4e7062a34ebe9bf982d37de0dba8428b592cc3
Kelvin Ferri Brancalhão <kelvinbrancalhao@gmail.com>
2021-09-23 21:55:03 -0300

    alteracao 1

--- Python/LearnPython_fullCourse/app.py
+++ Python/LearnPython_fullCourse/app.py
@@ -5,7 +5,10 @@ caracter_age = "35"
 
 print("the name of my caracter is " + caracter_name)
 print("the age of my caracter is " + caracter_age)
-print("")
+print(caracter_name.upper().isupper())
+print(len(caracter_name))
+print(caracter_name[0])
+
 print("")
 print("")
 print("")
